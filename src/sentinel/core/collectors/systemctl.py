a19c05f4ea289cf3f49a9713f4d3f4cb759035e1
chobahnmi <chobahnmi@ludmila.attlocal.net>
2026-09-20 09:50:39 -0500

    editing boiler plat

--- src/sentinel/core/collectors/systemctl.py
+++ src/sentinel/core/collectors/systemctl.py
@@ -1,11 +1,7 @@
 import logging
 
-
 logger = logging.getLogger(__name__)
 
-DEFAULT_MOUNTS = ["/", "/var", "/home"]
-
-def collect(path: str | None = None) :
+def collect() :
     
-        
     logger.info(f"system")    
