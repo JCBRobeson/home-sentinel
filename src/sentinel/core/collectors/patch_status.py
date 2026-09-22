eb6974a5e96cf4c703f28f3d632bd7cb21ec7664
chobahnmi <chobahnmi@ludmila.attlocal.net>
2026-09-22 06:26:52 -0500

    extending boiler plate

--- src/sentinel/core/collectors/patch_status.py
+++ src/sentinel/core/collectors/patch_status.py
@@ -10,5 +10,29 @@ logger = logging.getLogger(__name__)
 def collect() -> list[CheckResult]:
 
     results: list[CheckResult] = []
+    results.extend(_collect_package_updates())
+    results.extend(_collect_reboot_status())
+    results.extend(_collect_patch_staleness())
+
+    return results
+
+
+def _collect_package_updates() -> list[CheckResult]:
+
+    results: list[CheckResult] = []
+
+    return results
+
+
+def _collect_reboot_status() -> list[CheckResult]:
+
+    results: list[CheckResult] = []
+
+    return results
+
+
+def _collect_patch_staleness() -> list[CheckResult]:
+
+    results: list[CheckResult] = []
 
     return results
